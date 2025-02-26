f7c47f88d65f27dfe31e593ba3f807998b81189b
LimBoo233 <13720081217@163.com>
2025-02-26 22:04:59 +0800

    不赖的

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.Serialization;
 
 public class ClearCounter : MonoBehaviour {
 
+	[SerializeField] private KitchenObjectSO kithKitchenObjectSO;
+	[SerializeField] private Transform CounterTopPoint;
+	
 	public void Interact() {
-		Debug.Log("Interact");
+		Transform kitchenObjectTransform = Instantiate(kithKitchenObjectSO.getPrefab(), CounterTopPoint);
+		kitchenObjectTransform.localPosition = Vector3.zero;
+		
+		Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().getKitchenObjectSO().getName());
 	}
 }
